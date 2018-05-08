63ddbdfb6cd472ddf54976330ff57535a4c4db35
MARCUS DE MELO MOREIRA <marcus_de_melo_moreira@hotmail.com>
2018-05-07 22:07:32 -0300

    primeiro

---
+++ ProjDASC2018/src/main/java/br/univille/projdacs2018/ProjDasc2018Application.java
@@ -0,0 +1,12 @@
+package br.univille.projdacs2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjDasc2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjDasc2018Application.class, args);
+	}
+}
